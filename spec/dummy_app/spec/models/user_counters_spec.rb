9ca4bef1fca83f00d73e3b4d26c34ab382625ab7
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-07-13 20:41:08 +0400

    draft_counter_bug

--- spec/dummy_app/spec/models/user_counters_spec.rb
+++ spec/dummy_app/spec/models/user_counters_spec.rb
@@ -1,13 +1,36 @@
 require 'spec_helper'
 
+def destroy_all
+  User.destroy_all
+  Comment.destroy_all
+end
+
+def create_users_and_post
+  @user = FactoryGirl.create(:user)
+
+  @post_holder = FactoryGirl.create(:user)
+  @post        = FactoryGirl.create(:post, user: @post_holder)
+end
+
+def base_testing_situation
+  create_users_and_post
+
+  3.times do
+    @comment = @user.comments.create!(
+      commentable: @post,
+      title: Faker::Lorem.sentence,
+      raw_content: Faker::Lorem.paragraphs(3).join
+    )
+  end
+
+  @user.reload
+  @post_holder.reload
+end
+
 describe User do
   context 'User leave comment to the post' do
-    before(:all) do
-      @user = FactoryGirl.create(:user)
-
-      @post_holder = FactoryGirl.create(:user)
-      @post        = FactoryGirl.create(:post, user: @post_holder)
-    end
+    before(:all){ create_users_and_post }
+    after(:all){  destroy_all           }
 
     it 'should be User' do
       @user.should        be_instance_of User
@@ -35,25 +58,10 @@ describe User do
   end
 
   context 'User leave 3 comments and Instances has expectable counter values' do
-    before(:all) do
-      @user = FactoryGirl.create(:user)
+    before(:all){ base_testing_situation }
+    after(:all) { destroy_all            }
 
-      @post_holder = FactoryGirl.create(:user)
-      @post        = FactoryGirl.create(:post, user: @post_holder)
-
-      3.times do
-        @comment = @user.comments.create!(
-          commentable: @post,
-          title: Faker::Lorem.sentence,
-          raw_content: Faker::Lorem.paragraphs(3).join
-        )
-      end
-
-      @user.reload
-      @post_holder.reload
-    end
-
-    context 'User counters' do
+    describe 'User counters' do
       it 'my_comments counter' do
         @user.my_comments.count.should eq 3
         @user.my_comments_count.should eq 3
@@ -67,7 +75,7 @@ describe User do
       end
     end
 
-    context 'Holder counters' do
+    describe 'Holder counters' do
       it 'my_comments counter' do
         @post_holder.my_comments.count.should eq 0
         @post_holder.my_comments_count.should eq 0
@@ -77,6 +85,46 @@ describe User do
         @post_holder.comcoms.count.should           eq 3
         @post_holder.draft_comcoms_count.should     eq 3
         @post_holder.published_comcoms_count.should eq 0
+        @post_holder.deleted_comcoms_count.should   eq 0
+      end
+    end
+  end
+
+  context 'User leave 3 comments, states was changed, counters should have expectable values' do
+    before(:all) do
+      base_testing_situation
+      @comment = Comment.first.to_published
+
+      @user.reload
+      @post_holder.reload
+    end
+
+    after(:all) { destroy_all }
+
+    describe 'User counters' do
+      it 'has expectable values' do
+        @user.comments.count.should    eq 3
+        @user.my_comments_count.should eq 3
+        @user.comcoms.count.should     eq 0
+
+        @user.comments.with_state(:draft).count.should     eq 2
+        @user.comments.with_state(:published).count.should eq 1
+        @user.comments.with_state(:deleted).count.should   eq 0
+      end
+    end
+
+    describe 'Holder counters' do
+      it 'has expectable values' do
+        @post_holder.comments.count.should    eq 0
+        @post_holder.my_comments_count.should eq 0
+        @post_holder.comcoms.count.should     eq 3
+
+        @post_holder.comcoms.with_state(:draft).count.should     eq 2
+        @post_holder.comcoms.with_state(:published).count.should eq 1
+        @post_holder.comcoms.with_state(:deleted).count.should   eq 0
+
+        @post_holder.draft_comcoms_count.should     eq 2 # TODO! 3
+        @post_holder.published_comcoms_count.should eq 1
         @post_holder.deleted_comcoms_count.should   eq 0
       end
     end
--- spec/dummy_app/spec/spec_helper.rb
+++ spec/dummy_app/spec/spec_helper.rb
@@ -15,7 +15,8 @@ RSpec.configure do |config|
 
   # DatabaseCleaner
   config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
