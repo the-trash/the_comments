e89ee483ad10f62c6636b4004cec7cc1284abf33
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-07-13 19:47:29 +0400

    tests_on_create

--- spec/dummy_app/spec/models/user_counters_spec.rb
+++ spec/dummy_app/spec/models/user_counters_spec.rb
@@ -14,12 +14,12 @@ describe User do
       @post_holder.should be_instance_of User
     end
 
-    it 'should be Post' do
-      @post.should be_instance_of Post
-    end
-
     it 'Post should has owner' do
       @post.user.should eq @post_holder
+    end
+
+    it 'should be Post' do
+      @post.should be_instance_of Post
     end
 
     it 'Create new comment' do
@@ -31,11 +31,10 @@ describe User do
 
       @comment.user.should   eq @user
       @comment.holder.should eq @post_holder
-      @comment.should        be_instance_of Comment
     end
   end
 
-  context 'User leave comments and Instances has expectable counter values' do
+  context 'User leave 3 comments and Instances has expectable counter values' do
     before(:all) do
       @user = FactoryGirl.create(:user)
 
@@ -49,12 +48,37 @@ describe User do
           raw_content: Faker::Lorem.paragraphs(3).join
         )
       end
+
+      @user.reload
+      @post_holder.reload
     end
 
-    it 'User have expectable counter values' do
-      @user.comments.count.should    eq 3
-      @user.my_comments.count.should eq 3
-      @user.my_comments_count.should eq 3
+    context 'User counters' do
+      it 'my_comments counter' do
+        @user.my_comments.count.should eq 3
+        @user.my_comments_count.should eq 3
+      end
+
+      it 'Comcoms counters' do
+        @user.comcoms.count.should           eq 0
+        @user.draft_comcoms_count.should     eq 0
+        @user.published_comcoms_count.should eq 0
+        @user.deleted_comcoms_count.should   eq 0
+      end
+    end
+
+    context 'Holder counters' do
+      it 'my_comments counter' do
+        @post_holder.my_comments.count.should eq 0
+        @post_holder.my_comments_count.should eq 0
+      end
+
+      it 'Comcoms counters' do
+        @post_holder.comcoms.count.should           eq 3
+        @post_holder.draft_comcoms_count.should     eq 3
+        @post_holder.published_comcoms_count.should eq 0
+        @post_holder.deleted_comcoms_count.should   eq 0
+      end
     end
   end
 end
