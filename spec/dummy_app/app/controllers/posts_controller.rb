ae4abf03449775e2e0228a384b56e46a89b90b1e
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-10-29 10:51:49 +0400

    recent_comments_on_views

--- spec/dummy_app/app/controllers/posts_controller.rb
+++ spec/dummy_app/app/controllers/posts_controller.rb
@@ -1,6 +1,9 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @recent_comments = Comment.with_state(:published)
+                       .where(commentable_state: [:published])
+                       .recent.page(params[:page])
   end
 
   def show
