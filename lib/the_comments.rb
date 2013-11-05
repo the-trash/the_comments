d071a0e7d160efd56503fd4c536a8d9038a1eaff
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-11-05 12:01:58 +0400

    v2.1.0

--- lib/the_comments.rb
+++ lib/the_comments.rb
@@ -11,9 +11,9 @@ end
 # Loading of concerns
 
 # controllers
-require "../../app/controllers/concerns/controller"
+require File.expand_path('../../app/controllers/concerns/controller.rb', __FILE__)
 
 # models
 %w[ comment_states comment user commentable ].each do |concern|
-  require "../../app/models/concerns/#{concern}"
+  require File.expand_path("../../app/models/concerns/#{concern}.rb", __FILE__)
 end
