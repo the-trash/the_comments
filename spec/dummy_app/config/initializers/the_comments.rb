ca5deff60f373dea9a3e5732b591309aa81d62cf
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-11-05 08:20:07 +0300

    Update the_comments.rb

--- spec/dummy_app/config/initializers/the_comments.rb
+++ spec/dummy_app/config/initializers/the_comments.rb
@@ -2,11 +2,12 @@
 
 TheComments.configure do |config|
   config.max_reply_depth     = 3                   # comments tree depth
-  config.tolerance_time      = 3                   # sec - after this delay user can post a comment
+  config.tolerance_time      = 5                   # sec - after this delay user can post a comment
   config.default_state       = :draft              # default state for comment
   config.default_owner_state = :published          # default state for comment for Moderator
   config.empty_inputs        = [:commentBody]      # array of spam trap fields
   config.default_title       = 'Undefined title'   # default commentable_title for denormalization
+  config.template_engine     = :haml
 
   config.empty_trap_protection     = true
   config.tolerance_time_protection = true
