30581d2a0167fad2917ca82f82384ad596ba55cc
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-08-11 19:06:15 +0400

    ~~~

--- lib/the_comments.rb
+++ lib/the_comments.rb
@@ -1,4 +1,7 @@
 require 'state_machine'
+require 'state_machine/version'
+
+require 'the_simple_sort'
 require 'the_sortable_tree'
 
 require 'the_comments/config'
@@ -16,4 +19,21 @@ require "#{_root_}/app/controllers/concerns/controller.rb"
 
 %w[ comment_states comment user commentable ].each do |concern|
   require "#{_root_}/app/models/concerns/#{concern}.rb"
+end
+
+if StateMachine::VERSION.to_f <= 1.2
+  puts '~' * 50
+  puts 'TheComments'
+  puts '~' * 50
+  puts 'WARNING!'
+  puts 'StateMachine patch for Rails4 will be applied'
+  puts
+  puts '> private method *around_validation* from StateMachine::Integrations::ActiveModel will be public'
+  puts
+  puts 'https://github.com/pluginaweek/state_machine/issues/295'
+  puts 'https://github.com/pluginaweek/state_machine/issues/251'
+  puts '~' * 50
+  module StateMachine::Integrations::ActiveModel
+    public :around_validation
+  end
 end
