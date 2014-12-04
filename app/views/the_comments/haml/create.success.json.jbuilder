21f8a99b67d91f5e19571e938d20ada62bdfa34c
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-12-05 00:10:16 +0300

    ~~~

--- app/views/the_comments/haml/create.success.json.jbuilder
+++ app/views/the_comments/haml/create.success.json.jbuilder
@@ -1,2 +1,2 @@
 json.comment render(partial: comment_template('comment.html'), locals: { tree: @comment })
-json.comments_sum @commentable.comments_sum
+json.comments_sum @commentable.reload.comments_sum
--- spec/dummy_app/config/initializers/the_comments.rb
+++ spec/dummy_app/config/initializers/the_comments.rb
@@ -1,7 +1,7 @@
 # TheComments.config.param_name => value
 
 TheComments.configure do |config|
-  config.max_reply_depth     = 3                   # comments tree depth
+  config.max_reply_depth     = 5                   # comments tree depth
   config.tolerance_time      = 5                   # sec - after this delay user can post a comment
   config.default_state       = :draft              # default state for comment
   config.default_owner_state = :published          # default state for comment for Moderator
