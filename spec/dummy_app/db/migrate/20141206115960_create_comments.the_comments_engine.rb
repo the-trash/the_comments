ba0400a9c46079bab65a7b7ac9e9c57d00eba6ea
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-12-06 15:00:53 +0300

    new migrations

---
+++ spec/dummy_app/db/migrate/20141206115960_create_comments.the_comments_engine.rb
@@ -0,0 +1,51 @@
+# This migration comes from the_comments_engine (originally 20130101010102)
+class TheCommentsCreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      # relations
+      t.integer :user_id
+      t.integer :holder_id
+      
+      # polymorphic, commentable object
+      t.integer :commentable_id
+      t.string  :commentable_type
+
+      # denormalization
+      t.string  :commentable_url
+      t.string  :commentable_title
+      t.string  :commentable_state
+
+      # comment
+      t.string :anchor
+      
+      t.string :title
+      t.string :contacts
+
+      t.text :raw_content
+      t.text :content
+
+      # moderation token
+      t.string :view_token
+
+      # state machine => :draft | :published | :deleted
+      t.string :state, default: :draft
+
+      # base user data (BanHammer power)
+      t.string  :ip,             default: :undefined
+      t.string  :referer,        default: :undefined
+      t.string  :user_agent,     default: :undefined
+      t.integer :tolerance_time
+
+      # unusable: for future versions
+      t.boolean :spam, default: false
+
+      # nested set
+      t.integer :parent_id
+      t.integer :lft
+      t.integer :rgt
+      t.integer :depth, default: 0
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/dummy_app/db/migrate/20141206115962_add_antispam_services_fields.the_comments_engine.rb
@@ -0,0 +1,11 @@
+# This migration comes from the_comments_engine (originally 20130101010104)
+class TheCommentsAddAntispamServicesFields < ActiveRecord::Migration
+  def change
+    change_table :comments do |t|
+      t.string :akismet_state, default: :default
+
+      t.string :yandex_cleanweb_state, default: :default
+      t.string :yandex_cleanweb_id,    default: ''
+    end
+  end
+end
